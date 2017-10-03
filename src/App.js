062c6bfb4ebb99a6aa361c891e90dd2dcd2c236e
Rikesh Dhokia <rikeshdhokia@gmail.com>
2017-10-03 16:52:39 +1100

    Add tasks to README; clean up header; Add Trello container

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Trello from './Trello';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img className="App-logo" alt="logo" />
+          <h1 className="App-title">MyToDo</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Trello-container">
+          <Trello />
+        </div>
       </div>
     );
   }
---
+++ src/Trello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Trello.css';
+
+class Trello extends Component {
+  render() {
+    return (
+      <div className="Trello">
+        Board will live here
+      </div>
+    );
+  }
+}
+
+export default Trello;
