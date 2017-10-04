7a54b9aeb1f4559da7709f8b4519ad04557de289
Rikesh Dhokia <rikeshdhokia@gmail.com>
2017-10-04 11:50:30 +1100

    return stubbed put cards

--- src/TrelloApiService.js
+++ src/TrelloApiService.js
@@ -1,12 +1,8 @@
-// var jsonfile = require('jsonfile')
-// var file = '/tmp/data.json'
-// jsonfile.readFile(file, function(err, obj) {
-//   console.dir(obj)
-// })
+import mockCards from './cards.json';
 
 const getBoardCards = function(boardId) {
   console.log('TrelloApiService.getBoardCards: will fetch cards from ' + boardId);
-  return [{id: "abc"}, {id: "zxc"}];
+  return mockCards;
 };
 
 const TrelloApiService = {getBoardCards};
